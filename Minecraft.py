5d80ee5a9cd105c812bc1fb5fde03c8a8822b170
Joseph <jnjbros2@gmail.com>
2020-12-30 03:36:42 -0700

    Update Minecraft.py

--- Minecraft.py
+++ Minecraft.py
@@ -91,7 +91,7 @@ class minecraft(commands.Cog):
           category = ctx.channel.category
           jsonPath = "/root/discordbot/data/tpunbot/cogs/Minecraft/vcOwners.json"
           print("ran vc create function")
-          run : bool = true
+          run : bool = True
           if vcName == "":
                await ctx.send("You need to type a voice channel name t!vc create ['Name']")
                print("vcName is equal to None")
